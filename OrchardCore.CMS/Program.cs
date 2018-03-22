e4573626d177d30f35beb1c1ef5becae80bbb8c1
Jasmin Savard <jasmin.savard@gmail.com>
2018-03-22 13:42:45 -0400

    Fixing Typo
    renaming OchardCore to OrchardCore

--- OrchardCore.CMS/Program.cs
+++ OrchardCore.CMS/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace OchardCore.NowUI
+namespace OrchardCore.CMS
 {
     public class Program
     {
--- OrchardCore.CMS/Startup.cs
+++ OrchardCore.CMS/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace OchardCore.NowUI
+namespace OrchardCore.CMS
 {
     public class Startup
     {

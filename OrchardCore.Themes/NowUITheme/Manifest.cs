12c5fdab7386ff68c982a18c00e499bf5c54429e
Jasmin Savard <jasmin.savard@gmail.com>
2018-03-22 21:02:55 -0400

    removing NowUITheme

--- OrchardCore.Themes/NowUITheme/Manifest.cs
+++
@@ -1,9 +0,0 @@
-using OrchardCore.DisplayManagement.Manifest;
-
-[assembly: Theme(
-    Name = "Now UI Theme",
-    Author = "The Orchard Team",
-    Website = "http://orchardproject.net",
-    Version = "2.0.0",
-    Description = "Now UI Theme Kit."
-)]

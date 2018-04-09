69576cf7a517ccafe5f3750347c325865cb0f72d
Jasmin Savard <jasmin.savard@gmail.com>
2018-04-09 12:42:59 -0400

    Adding back startup.cs file for template module

---
+++ OrchardCore.Modules/TemplateModule.OrchardCore/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+#if (AddPart)
+using TemplateModule.OrchardCore.Drivers;
+using TemplateModule.OrchardCore.Handlers;
+using TemplateModule.OrchardCore.Models;
+using TemplateModule.OrchardCore.Settings;
+#endif
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+#if (AddPart)
+using OrchardCore.ContentManagement;
+using OrchardCore.ContentManagement.Display.ContentDisplay;
+using OrchardCore.ContentManagement.Handlers;
+using OrchardCore.ContentTypes.Editors;
+using OrchardCore.Data.Migration;
+#endif
+using OrchardCore.Modules;
+
+namespace TemplateModule.OrchardCore
+{
+    public class Startup : StartupBase
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+#if (AddPart)
+            services.AddScoped<IContentPartDisplayDriver, MyTestPartDisplayDriver>();
+            services.AddSingleton<ContentPart, MyTestPart>();
+            services.AddScoped<IContentPartDefinitionDisplayDriver, MyTestPartSettingsDisplayDriver>();
+            services.AddScoped<IDataMigration, Migrations>();
+            services.AddScoped<IContentPartHandler, MyTestPartHandler>();
+#endif
+        }
+
+        public override void Configure(IApplicationBuilder builder, IRouteBuilder routes, IServiceProvider serviceProvider)
+        {
+        }
+    }
+}
